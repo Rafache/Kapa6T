036d3289edeeb782f766f014f40648331c4c6e96
Raphael Chemin <raphael.chemin@groupebayard.com>
2026-09-09 15:59:20 +0200

    build(vite): configure dev and preview server for reverse proxy

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,17 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  server: {
+    host: process.env.HOST || '192.168.1.10',
+    port: 5174,
+    allowedHosts: ['.aubox.chem1.fr'],
+    hmr: {
+      clientPort: 443,
+    },
+  },
+  preview: {
+    host: process.env.HOST || '192.168.1.10',
+    port: 5174,
+    allowedHosts: ['.aubox.chem1.fr'],
+  },
 });
